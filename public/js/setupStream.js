30cab65e8a1a9424528b4d9492ad608f862c7ef9
Minitsonga <71793112+Minitsonga@users.noreply.github.com>
2022-09-28 01:05:36 +0200

    update

---
+++ public/js/setupStream.js
@@ -0,0 +1,43 @@
+var isOnline;
+
+async function getData() {
+  await fetch(
+    "https://wapi.wizebot.tv/api/channels/c476492c15def796d57a7f0952228757/datas",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      cache: "default",
+    }
+  )
+    .then((res) => res.json())
+    .then((json_data) => (isOnline = json_data.data.online));
+
+  console.log("ici " + isOnline);
+
+  const streamOverlay = document.querySelector("#stream");
+  const chatOverlay = document.querySelector("#chat-holder");
+  const imgOffline = document.querySelector(".img");
+
+  if (isOnline > 0) {
+    if (!streamOverlay.classList.contains("hide"))
+      streamOverlay.classList.add("hide");
+    if (!chatOverlay.classList.contains("hide"))
+      chatOverlay.classList.add("hide");
+    if (imgOffline.classList.contains("hide"))
+      imgOffline.classList.remove("hide");
+    document.getElementById("holderTotal").style = "";
+  } else {
+    if (streamOverlay.classList.contains("hide"))
+      streamOverlay.classList.remove("hide");
+    if (chatOverlay.classList.contains("hide"))
+      chatOverlay.classList.remove("hide");
+    if (!imgOffline.classList.contains("hide"))
+      imgOffline.classList.add("hide");
+  }
+}
+
+getData();
