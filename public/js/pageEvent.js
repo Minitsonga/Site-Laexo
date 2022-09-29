3d9562293aa9a97d0de2257b0d24f1dfa54afdac
Minitsonga <71793112+Minitsonga@users.noreply.github.com>
2022-09-29 02:28:56 +0200

    Dynamic event page done

--- public/js/pageEvent.js
+++ public/js/pageEvent.js
@@ -1,5 +1,32 @@
 const holder = document.querySelector("#holder");
 
-var tag = document.createElement("p"); // <p></p>
-tag.innerHTML="Coucou je suis ton pere"
-holder.appendChild(tag);
+var titleDiv = document.createElement("div");
+titleDiv.classList.add("title");
+titleDiv.innerHTML = title;
+
+var subtitleDiv = document.createElement("div");
+subtitleDiv.classList.add("subtitle");
+subtitleDiv.innerHTML = subtitle;
+
+var imgDiv = document.createElement("div");
+var imgElement = document.createElement("img");
+imgElement.src = "/img/" + img;
+imgElement.style = "height: auto; width: 50%";
+imgDiv.classList.add("pt-3");
+imgDiv.classList.add("center");
+imgDiv.appendChild(imgElement);
+
+
+var descriptionTitleDiv = document.createElement("div");
+descriptionTitleDiv.classList.add("description-title");
+descriptionTitleDiv.innerHTML = "DESCRIPTION";
+
+var descriptionDiv = document.createElement("div");
+descriptionDiv.classList.add("description");
+descriptionDiv.innerHTML = description;
+
+holder.appendChild(titleDiv);
+holder.appendChild(subtitleDiv);
+holder.appendChild(imgDiv);
+holder.appendChild(descriptionTitleDiv);
+holder.appendChild(descriptionDiv);
