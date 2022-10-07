5ab7b93bae9e6b5d4746a613823e557c3485a35e
Minitsonga <minitsonga2002@gmail.com>
2022-10-07 14:44:48 +0200

    Update routing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const oauth = require('./oauth');
+const stream = require('./mainWebSite/stream');
+const classement = require('./mainWebSite/classement');
+const events = require('./mainWebSite/events');
+const planning = require('./mainWebSite/planning');
+
 
 router.use('/oauth', oauth);
+router.use('/stream', stream);
+router.use('/classement', classement);
+router.use('/events', events);
+router.use('/planning', planning);
+
 
 module.exports = router;
