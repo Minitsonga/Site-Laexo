15f37752e34b009cc3cf609e2942cc807b49037a
Minitsonga <71793112+Minitsonga@users.noreply.github.com>
2022-10-09 00:12:15 +0200

    Remove acces login for main branch

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,21 @@
-const router = require('express').Router();
-const oauth = require('./oauth');
-const stream = require('./mainWebSite/stream');
-const classement = require('./mainWebSite/classement');
-const events = require('./mainWebSite/events');
-const planning = require('./mainWebSite/planning');
-const aboutme = require('./mainWebSite/quisuisje');
+const router = require("express").Router();
+const oauth = require("./oauth");
+const stream = require("./mainWebSite/stream");
+const classement = require("./mainWebSite/classement");
+const events = require("./mainWebSite/events");
+const planning = require("./mainWebSite/planning");
+const conception = require("./mainWebSite/conception");
+const aboutme = require("./mainWebSite/quisuisje");
 
-const admin = require('./backOffice/admin');
+const admin = require("./backOffice/admin");
 
-
-router.use('/oauth', oauth);
-router.use('/stream', stream);
-router.use('/classement', classement);
-router.use('/events', events);
-router.use('/planning', planning);
-router.use('/admin', admin);
-router.use('/quisuisje', aboutme);
-
+router.use("/oauth", oauth);
+router.use("/stream", stream);
+router.use("/classement", classement);
+router.use("/events", events);
+router.use("/planning", planning);
+router.use("/conception", conception);
+//router.use('/admin', admin);
+router.use("/quisuisje", aboutme);
 
 module.exports = router;
