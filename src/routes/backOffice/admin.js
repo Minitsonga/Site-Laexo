51fab9a7fe100d76c319c29a59dff5c929f856f9
Minitsonga <71793112+Minitsonga@users.noreply.github.com>
2022-10-08 02:14:38 +0200

    Update Login page

--- src/routes/backOffice/admin.js
+++ src/routes/backOffice/admin.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
 
 router.get("/events/login", (req,res) =>{
-    res.render("pages/login");
+    let visible = false;
+    res.render("pages/login", {visible});
+});
+
+router.post("/events/login", (req,res) =>{
+    let {userName, password} = req.body;
+    console.log(req.body);
+
+    if(userName == process.env.ADMIN_ID && password == process.env.ADMIN_PASSWORD)
+    {
+        console.log("C bon");
+        res.redirect("/admin/events/editor");
+    }else
+    {
+        console.log("pas bon");
+        res.sendStatus(400);
+        
+    }
+
 });
 
 module.exports = router;
