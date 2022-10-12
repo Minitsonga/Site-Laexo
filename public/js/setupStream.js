23f9deeee88a9e5c31c30d6c078b3b3c562f586e
Minitsonga <minitsonga2002@gmail.com>
2022-10-12 19:11:06 +0200

    Updating navigator css

--- public/js/setupStream.js
+++ public/js/setupStream.js
@@ -1,41 +1,41 @@
-var isOnline;
+// var isOnline;
 
-async function getData() {
-  await fetch(
-    "https://wapi.wizebot.tv/api/channels/c476492c15def796d57a7f0952228757/datas",
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      mode: "cors",
-      cache: "default",
-    }
-  )
-    .then((res) => res.json())
-    .then((json_data) => (isOnline = json_data.data.online));
+// async function getData() {
+//   await fetch(
+//     "https://wapi.wizebot.tv/api/channels/c476492c15def796d57a7f0952228757/datas",
+//     {
+//       method: "GET",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//       },
+//       mode: "cors",
+//       cache: "default",
+//     }
+//   )
+//     .then((res) => res.json())
+//     .then((json_data) => (isOnline = json_data.data.online));
 
 
-  const streamOverlay = document.querySelector("#stream");
-  const imgOffline = document.querySelector(".img");
+//   const streamOverlay = document.querySelector("#stream");
+//   const imgOffline = document.querySelector(".img");
 
-  if (isOnline <= 0) {
-    if (!streamOverlay.classList.contains("hide"))
-      streamOverlay.classList.add("hide");
-    // if (!chatOverlay.classList.contains("hide"))
-    //   chatOverlay.classList.add("hide");
-    if (imgOffline.classList.contains("hide"))
-      imgOffline.classList.remove("hide");
-    document.getElementById("holderTotal").style = "";
-  } else {
-    if (streamOverlay.classList.contains("hide"))
-      streamOverlay.classList.remove("hide");
-    // if (chatOverlay.classList.contains("hide"))
-    //   chatOverlay.classList.remove("hide");
-    if (!imgOffline.classList.contains("hide"))
-      imgOffline.classList.add("hide");
-  }
-}
+//   if (isOnline <= 0) {
+//     if (!streamOverlay.classList.contains("hide"))
+//       streamOverlay.classList.add("hide");
+//     // if (!chatOverlay.classList.contains("hide"))
+//     //   chatOverlay.classList.add("hide");
+//     if (imgOffline.classList.contains("hide"))
+//       imgOffline.classList.remove("hide");
+//     document.getElementById("holderTotal").style = "";
+//   } else {
+//     if (streamOverlay.classList.contains("hide"))
+//       streamOverlay.classList.remove("hide");
+//     // if (chatOverlay.classList.contains("hide"))
+//     //   chatOverlay.classList.remove("hide");
+//     if (!imgOffline.classList.contains("hide"))
+//       imgOffline.classList.add("hide");
+//   }
+// }
 
-getData();
+// getData();
