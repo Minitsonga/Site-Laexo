10088e900942090e1b858fc139d380f47c175f74
Minitsonga <minitsonga2002@gmail.com>
2022-10-17 09:45:04 +0200

    CHange admin route

--- public/js/editorManager.js
+++ public/js/editorManager.js
@@ -1,5 +1,5 @@
+const select = document.querySelector(select);
 
-
-
+console.log(select);
 
 // console.log((2100/900 == 1200/450)); Check if 2 resolution have the same ratio
--- src/routes/backOffice/admin.js
+++ src/routes/backOffice/admin.js
@@ -2,34 +2,34 @@ const router = require("express").Router();
 
 let visible = false;
 
-router.get("/events/login", (req, res) => {
+router.get("/login", (req, res) => {
   res.render("pages/login", { visible });
   visible = false;
 });
 
-router.post("/events/login", (req, res) => {
+router.post("/login", (req, res) => {
   let { userName, password } = req.body;
   console.log(req.body);
 
   if (userName == process.env.ADMIN_ID && password == process.env.ADMIN_PASSWORD) {
     req.session.loggedin = true;
     visible = false;
-    res.redirect("/admin/events/editor");
+    res.redirect("/admin/editor");
   } else {
     visible = true;
-    res.redirect("/admin/events/login");
+    res.redirect("/admin/login");
   }
 
   res.end();
 });
 
-router.get("/events/editor", (req, res) => {
+router.get("/editor", (req, res) => {
   if (req.session.loggedin) {
     console.log("Editor");
     res.render("pages/editor");
   } else {
     console.log("dommage");
-    res.redirect("/admin/events/login");
+    res.redirect("/admin/login");
   }
 });
 
